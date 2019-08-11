3bbd5d0d12f6fa2b782d26a62bed74f8e1d789dd
lggruspe <mail.levig@gmail.com>
2019-08-11 16:36:42 +0800

    Add install_requires

--- python/setup.py
+++ python/setup.py
@@ -5,14 +5,15 @@ with open("../README.md", "r") as f:
 
 setuptools.setup(
     name="rnd",
-    version="0.2a",
+    version="0.2a1",
     author="Levi Gruspe",
     author_email="mail.levig@gmail.com",
     description="Python bindings for libcrnd",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lggruspe/rnd",
-    packages=setuptools.find_packages(),
+    packages=["rnd"],
+    install_requires=["redblack"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
