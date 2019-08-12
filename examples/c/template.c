424a9ac8710f93e4ebff46439e30cff861244f38
lggruspe <mail.levig@gmail.com>
2019-08-12 20:47:32 +0800

    Make c example compile

--- examples/c/template.c
+++ examples/c/template.c
@@ -1,5 +1,6 @@
 #include "vector.h"
-#include "sw_list.h"
+#include "swlist/swlist.h"
+#include <stdio.h>
 
 #define fail_if_empty(input) do {\
     int c = (input);\
@@ -31,14 +32,16 @@ sw_register(scanner)
 
 int main()
 {
-    struct vector_int lexeme = NULL;
+    struct vector_int *lexeme = NULL;
     struct sw_list_scanner *scanners = NULL;
-## for scanner in scanners
-    sw_push(scanners, (scanner) {
+## for scanner in scanners|reverse
+    scanners = sw_push(scanners, sw_create((scanner) {
         .token = token_{{ scanner.token|upper }},
         .state = -1,
         .start = 0,
         .transition = transition_{{ scanner.token }}
-    });
+    }));
 ## endfor
+
+    sw_destroy(scanners);
 }
