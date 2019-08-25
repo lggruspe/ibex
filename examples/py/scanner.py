8229a605d965a6fac2a7a5e3c76bb5aa3cabe865
lggruspe <mail.levig@gmail.com>
2019-08-25 12:49:34 +0800

    Modify python example

--- examples/py/scanner.py
+++ examples/py/scanner.py
@@ -1,19 +1,19 @@
 class {{ scanner.token|title }}Scanner(Scanner):
     def __init__(self):
-        super().__init__()
+        super().__init__("{{ scanner.token }}")
 
 ## for state in scanner.transitions
     def s{{ state }}(self, char):
         if not char:
-            self.fail()
+            return self.fail()
 ## for transition in scanner.transitions[state]
 ## set start = transition[0].start
 ## set end = transition[0].end
 ## set next_state = transition[1]
         elif {{ start }} <= char <= {{ end }}:
             self.state = {{ next_state }}
+            return self.state
 ## endfor
-        else:
-            self.fail()
+        return self.fail()
 
 ## endfor
--- examples/py/template.py
+++ examples/py/template.py
@@ -1,59 +1,101 @@
 #!/usr/bin/env python3
+from collections import deque
 import sys
 
-def get_input():
+INPUT_STACK = []
+
+def io_get():
+    if INPUT_STACK:
+        return INPUT_STACK.pop()
+    char = sys.stdin.read(1)
+    if not char:
+        raise StopIteration
+    return ord(char)
+
+def io_unget(char):
+    INPUT_STACK.append(char)
+
+def io_iterate():
     while True:
-        char = sys.stdin.read(1)
-        if not char:
-            raise StopIteration
-        yield ord(char)
+        yield io_get()
 
 class Scanner:
-    def __init__(self):
-        self.start = 0
-        self.state = self.start
+    def __init__(self, token):
+        self.state = 0
+        self.checkpoint = 0
+        self.token = token
 
     def is_error(self):
         return self.state == -1
 
     def reset(self):
-        self.state = self.start
+        self.state = 0
 
     def fail(self):
         self.state = -1
+        return False
 
     def next(self, char):
+        if not char:
+            return self.fail()
         assert not self.is_error()
         try:
             transition = getattr(self, f"s{self.state}")
             transition(char)
+            return True
         except AttributeError:
-            self.fail()
+            return self.fail()
+
+    def backtrack(self):
+        # can backtrack one step
+        self.state = self.checkpoint
+        self.checkpoint = -1
+
+    def __repr__(self):
+        return f"<Scanner {self.token} state:{self.state} checkpoint:{self.checkpoint}>"
 
 ## for scanner in scanners
 ## include "scanner.py"
 ## endfor
 
-def main():
-    scanners = [
+def scan():
+    """Assume each scanner starts in a valid state."""
+    scanners = deque([
 ## for scanner in scanners
         {{ scanner.token|title }}Scanner(),
 ## endfor
-    ]
+    ])
     token = None
     lexeme = ""
-    for char in get_input():
-        if not scanners:
-            break
-        for i, scanner in enumerate(scanners):
-            if not scanner.is_error():
-                scanner.next(char)
-            else:
-                del scanners[i]
+    if not scanners:
+        return token, lexeme
 
-    # TODO rollback
+    # even if there are multiple matches, the input will eventually run out
+    # when the loop breaks, scanners[0] is matched
+    while len(scanners) > 1:
+        try:
+            char = io_get()
+        except StopIteration:
+            break
+        lexeme += chr(char)
+        scanner = scanners.popleft()
+        scanner.next(char)
+        if not scanner.is_error():
+            scanners.append(scanner)
+
+    # proceed scanning solo, rollback if needed
+    scanner = scanners[0]
+    token = scanner.token
+    for char in io_iterate():
+        lexeme += chr(char)
+        scanner.next(char)
+        if scanner.is_error():
+            lexeme = lexeme[:-1]
+            scanner.backtrack()
+            break
     return token, lexeme
 
 if __name__ == "__main__":
-    _, lexeme = main()
-    print(lexeme)
+    token, lexeme = scan()
+    print("Token:", token)
+    print("Lexeme:", lexeme)
