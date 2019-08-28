c0e6e49041cb66a401d076d8ce88f58cff383fc3
lggruspe <mail.levig@gmail.com>
2019-08-28 19:13:07 +0800

    Port longest match to c++

--- examples/cpp/test/match.hpp
+++ examples/cpp/test/match.hpp
@@ -1,7 +1,10 @@
 #pragma once
+#include <algorithm>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <utility>
+#include <vector>
 
 namespace match {
 
@@ -29,4 +32,53 @@ std::string single(std::string lexeme="")
 {
     return fsingle<Scanner>(std::cin, lexeme);
 }
+
+template <class Token, class BaseScanner, class... Scanners>
+std::pair<Token, std::string> flongest(std::istream& in)
+{
+    if constexpr(sizeof...(Scanners) == 0) {
+        return {Token(), ""};
+    }
+
+    std::vector<std::shared_ptr<BaseScanner>> scanners = { std::make_shared<Scanners>() ... };
+
+    Token record_token;
+    std::string record_lexeme;
+    for (auto& scanner_ptr: scanners) {
+        std::string lexeme;
+        for (int c = in.get(); c != EOF; c = in.get()) {
+            lexeme += (char)c;
+            if (!scanner_ptr->next(c)) {
+                for (auto it = lexeme.rbegin(); it != lexeme.rend(); ++it) {
+                    in.putback((int)(*it));
+                }
+                for (int i = 0; i < scanner_ptr->backtrack_steps(); ++i) {
+                    lexeme.pop_back();
+                }
+                if (scanner_ptr->accepts) {
+                    if (lexeme.size() > record_lexeme.size()) {
+                        record_token = scanner_ptr->token;
+                        record_lexeme = lexeme;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    if (!record_lexeme.empty()) {
+        std::for_each(record_lexeme.begin(), record_lexeme.end(),
+                [&in](const auto&) {
+                    in.get();
+                });
+    }
+    return {record_token, record_lexeme};
+}
+
+template <class Token, class BaseScanner, class... Scanners>
+std::pair<Token, std::string> longest()
+{
+    return flongest<Token, BaseScanner, Scanners...>(std::cin);
+}
+
 }
--- examples/cpp/test/run.cpp
+++ examples/cpp/test/run.cpp
@@ -5,8 +5,14 @@
 
 int main()
 {
+    /*
     std::string lexeme = match::single<NumberScanner>();
     std::string token = (lexeme.empty() ? "SyntaxError" : "Number");
+    std::cout << "token: " << token << std::endl;
+    std::cout << "lexeme: " << lexeme << std::endl;
+    */
+    
+    auto [token, lexeme] = match::longest<Token, Scanner, NumberScanner, IdentifierScanner>();
     std::cout << "token: " << token << std::endl;
     std::cout << "lexeme: " << lexeme << std::endl;
 }
