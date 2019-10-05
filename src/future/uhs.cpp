6e4b03d8be560858e982f95d85d39c8c4ed220d9
lggruspe <mail.levig@gmail.com>
2019-10-05 12:00:37 +0800

    Copy uhs into future

--- src/future/uhs.cpp
+++ src/future/uhs.cpp
@@ -1,50 +1,6 @@
+#include "uhs.hpp"
 #include <iostream>
-#include <vector>
-#include <map>
 #include <set>
-#include <utility>
-
-template <class T>
-class UniqueHandleSet {
-    using Map = std::map<T, int>;
-    using Vector = std::vector<typename Map::iterator>;
-    Map map;
-    Vector vector;
-
-public:
-    UniqueHandleSet() {}
-
-    int index(const T& t)
-    {
-        auto [it, ok] = map.insert({t, vector.size()});
-        if (!ok) {
-            return it->second;
-        }
-        int handle = vector.size();
-        vector.push_back(it);
-        return handle;
-    }
-
-    const T& value(int ind) const
-    {
-        return vector[ind]->first;
-    }
-
-    auto begin() const
-    {
-        return map.begin();
-    }
-
-    auto end() const
-    {
-        return map.end();
-    }
-
-    bool contains(const T& val) const
-    {
-        return map.find(val) != map.end();
-    }
-};
 
 int main()
 {
