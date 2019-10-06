aec7497a8c053ede7eef953bd14c67dcec4ac0a3
lggruspe <mail.levig@gmail.com>
2019-10-06 14:12:33 +0800

    Print union, concatenation and closure in dfa.test

--- src/future/dfa.test.cpp
+++ src/future/dfa.test.cpp
@@ -3,12 +3,41 @@
 #include "cis.hpp"
 #include <iostream>
 
-int main()
+void test_union()
 {
+    std::cout << "union" << std::endl;
     Expr a(ClosedInterval(65, 65));
-    //Expr expr = alternate(a, b);
+    Expr b(ClosedInterval(66, 66));
+    Expr expr = alternate(a, b);
+    std::cout << expr << std::endl;
+    Fsm fsm(expr);
+    std::cout << fsm << std::endl;
+}
+
+void test_concatenation()
+{
+    std::cout << "concatenation" << std::endl;
+    Expr a(ClosedInterval(65, 65));
+    Expr b(ClosedInterval(66, 66));
+    Expr expr = concatenate(a, b);
+    std::cout << expr << std::endl;
+    Fsm fsm(expr);
+    std::cout << fsm << std::endl;
+}
+
+void test_closure()
+{
+    std::cout << "closure" << std::endl;
+    Expr a(ClosedInterval(65, 65));
     Expr expr = closure(a);
     std::cout << expr << std::endl;
     Fsm fsm(expr);
     std::cout << fsm << std::endl;
+}
+
+int main()
+{
+    test_union();
+    test_concatenation();
+    test_closure();
 }
