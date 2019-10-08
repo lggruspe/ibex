1b7d64eee06005b434732d0db285237de9c68925
lggruspe <mail.levig@gmail.com>
2019-10-08 14:14:10 +0800

    Delete thompson.h

--- src/future/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "thompson.h"
-
-int main()
-{
-    auto a = Expr(SymbolInterval(1, 2));
-    auto b = Expr(SymbolInterval(2, 3));
-    auto c = concatenate(a, b);
-    std::cout << c << std::endl;
-}
