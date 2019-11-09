3e247ecd238c5183036b0b94e155d1e447fb7c30
lggruspe <mail.levig@gmail.com>
2019-11-09 22:58:29 +0800

    Copy handles.hpp to poc dir

---
+++ src/poc/handles.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <map>
+#include <utility>
+#include <vector>
+
+template <class T>
+class HandleSet {
+    using Map = std::map<T, int>;
+    using Vector = std::vector<typename Map::iterator>;
+    Map map;
+    Vector vector;
+
+public:
+    std::pair<int, bool> index(const T& t)
+    {
+        auto [it, ok] = map.insert({t, vector.size()});
+        if (!ok) {
+            return {it->second, false};
+        }
+        int handle = vector.size();
+        vector.push_back(it);
+        return {handle, true};
+    }
+
+    int index_only(const T& t)
+    {
+        auto [id, _] = index(t);
+        return id;
+    }
+
+    const T& value(int ind) const
+    {
+        return vector[ind]->first;
+    }
+
+    auto begin() const
+    {
+        return map.begin();
+    }
+
+    auto end() const
+    {
+        return map.end();
+    }
+
+    bool contains(const T& val) const
+    {
+        return map.find(val) != map.end();
+    }
+};
