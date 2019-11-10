00e184b8760a87feee654fe54353f99a7ef6d5f2
lggruspe <mail.levig@gmail.com>
2019-11-10 19:04:39 +0800

    Print output in poc/automaton.test

--- src/poc/automaton.test.cpp
+++ src/poc/automaton.test.cpp
@@ -1,6 +1,25 @@
 #include "automaton.hpp"
+#include "nexpr.hpp"
+#include "zsymbols.hpp"
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& os, const Automaton& fsm)
+{
+    for (const auto& [q, dq]: fsm.states) {
+        for (const auto& [a, r]: dq) {
+            os << "d(" << q << ", [" << a.start << ", " << a.end << ")) = " << r << std::endl;
+        }
+    }
+    os << "accepts: ";
+    for (const auto& f: fsm.accepts) {
+        os << f << " ";
+    }
+    return os;
+}
 
 int main()
 {
-
+    NExpr expr(ZRange('a', 'a'+1));
+    Automaton fsm(expr);
+    std::cout << fsm << std::endl;
 }
