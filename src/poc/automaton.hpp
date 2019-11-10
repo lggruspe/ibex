2891a8b923b844572a05843932554e8b972bf331
lggruspe <mail.levig@gmail.com>
2019-11-10 18:16:05 +0800

    Epsilon closures (poc)

--- src/poc/automaton.hpp
+++ src/poc/automaton.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include "zsymbols.hpp"
+#include "handles.hpp"
 #include "nexpr.hpp"
+#include "zsymbols.hpp"
 #include <map>
 #include <set>
 
@@ -13,3 +14,31 @@ struct Automaton {
 
     Automaton(const NExpr& expr);
 };
+
+auto epsilon_closures(const NExpr& expr)
+{
+    std::map<int, std::set<int>> closures;
+    std::set<int> queue;
+    for (const auto& [q, _]: expr.states) {
+        closures[q].insert(q);
+        queue.insert(q);
+    }
+    ZRange eps;
+    while (!queue.empty()) {
+        int q = *(queue.begin());
+        queue.erase(queue.begin());
+        for (const auto& [p, dp]: expr.states) {
+            if (auto it = dp.find(eps); it == dp.end()) {
+                continue;
+            } else if (it->second.count(q)) {
+                auto before = closures[p].size();
+                closures[p].insert(closures[q].begin(), closures[q].end());
+                auto after = closures[p].size();
+                if (before != after) {
+                    queue.insert(p);
+                }
+            }
+        }
+    }
+    return closures;
+}
