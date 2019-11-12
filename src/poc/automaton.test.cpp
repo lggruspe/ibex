00af895381e67965e6335b8eda75685051a103a1
lggruspe <mail.levig@gmail.com>
2019-11-12 13:33:12 +0800

    Print closure, alternate and concatenate output (poc)

--- src/poc/automaton.test.cpp
+++ src/poc/automaton.test.cpp
@@ -33,10 +33,41 @@ std::ostream& operator<<(std::ostream& os, const Automaton& fsm)
     return os;
 }
 
-int main()
+void test_closure()
 {
+    std::cout << "closure" << std::endl;
     NExpr expr(ZRange('a', 'a'+1));
+    expr = closure(expr);
     std::cout << expr << std::endl;
     Automaton fsm(expr);
     std::cout << fsm << std::endl;
+}
+
+void test_alternate()
+{
+    std::cout << "alternate" << std::endl;
+    NExpr expr(ZRange('a', 'a'+1));
+    NExpr other(ZRange('b', 'b'+1));
+    expr = alternate(expr, other);
+    std::cout << expr << std::endl;
+    Automaton fsm(expr);
+    std::cout << fsm << std::endl;
+}
+
+void test_concatenate()
+{
+    std::cout << "concatenate" << std::endl;
+    NExpr expr(ZRange('a', 'a'+1));
+    NExpr other(ZRange('b', 'b'+1));
+    expr = concatenate(expr, other);
+    std::cout << expr << std::endl;
+    Automaton fsm(expr);
+    std::cout << fsm << std::endl;
+}
+
+int main()
+{
+    test_closure();
+    test_alternate();
+    test_concatenate();
 }
