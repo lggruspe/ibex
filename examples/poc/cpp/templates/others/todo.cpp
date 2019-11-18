a5cb915cd623eaec3de2f9901b371a33e0569ec0
lggruspe <mail.levig@gmail.com>
2019-11-18 23:16:31 +0800

    Add c++ template ideas

---
+++ examples/poc/cpp/templates/others/todo.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+enum class Token {
+    INVALID = 0,
+    IDENTIFIER,
+    INTEGER,
+};
+
+struct BaseRecognizer {
+    Token token;
+
+    BaseRecognizer(
+        Token token = Token::INVALID)
+        : token(token)
+    {}
+
+    virtual std::pair<int, int> next(int q, uint32_t a) const = 0;
+
+    virtual std::pair<bool, std::string>
+    match(std::istream& in = std::cin) const = 0;
+};
+
+struct Identifier: public BaseRecognizer {
+    int const error;
+    uint32_t eof;
+    bool accept;
+
+    Identifier()
+        : BaseRecognizer(Token::IDENTIFIER)
+        , error(2)
+        , eof(std::char_traits<char>::eof())
+        , accept(false) // true if 0 is an accept state
+    {}
+
+    std::pair<int, int> next(int q, uint32_t a) const
+    {
+        switch (q) {
+        case 0:
+            if ('a' <= a && a < 'z'+1) {
+                return {1, 1};
+            }
+            if ('A' <= a && a < 'Z'+1) {
+                return {1, 1};
+            }
+            if ('_' == a) {
+                return {1, 1};
+            }
+            return {-1, 2};
+        case 1:
+            if ('a' <= a && a < 'z'+1) {
+                return {1, 1};
+            }
+            if ('A' <= a && a < 'Z'+1) {
+                return {1, 1};
+            }
+            if ('_' == a) {
+                return {1, 1};
+            }
+            if ('0' <= a && a <'9'+1) {
+                return {1, 1};
+            }
+            return {-1, 2};
+        default:
+            return {-1, 2};
+        }
+    }
+
+    std::pair<bool, std::string> match(std::istream& in = std::cin)
+    {
+        std::vector<int> checkpoint = {0};
+        std::vector<uint32_t> lexeme;
+        uint32_t a = -1;
+        while (checkpoint.back() != error && (a = in.get()) != eof) {
+            auto [status, r] = next(checkpoint.back(), a);
+            if (status == 1) {
+                accept = true;
+                checkpoint.clear();
+            }
+            checkpoint.push_back(r);
+            lexeme.push_back(a);
+        }
+        for (int i = 1; i < checkpoint.size(); ++i) {
+            // rollback to most recent accept state, if any
+            // (if accept is true, checkpoint[0] is the most recent accept state)
+            in.unget();
+            lexeme.pop_back();
+        }
+        return { accept, std::string(lexeme.begin(), lexeme.end()) };
+    }
+};
+
+template <class... Recognizers>
+std::pair<Token, std::string> match_first(std::istream& in = std::cin)
+{
+    std::vector<std::unique_ptr<BaseRecognizer>> recs = {
+        std::make_unique<Recognizers>() ...
+    };
+    for (auto r: recs) {
+        auto [ok, s] = r->match(in);
+        if (ok) {
+            return {r->token, s};
+        }
+    }
+    return {Token::INVALID, ""};
+}
+
+template <class... Recognizers>
+std::pair<Token, std::string> match_longest(std::istream& in = std::cin)
+{
+    std::vector<std::unique_ptr<BaseRecognizer>> recs = {
+        std::make_unique<Recognizers>() ...
+    };
+    Token token = Token::INVALID;
+    std::string lexeme;
+    for (auto r: recs) {
+        auto [ok, s] = r->match(in);
+        if (ok && s.size() > lexeme.size()) {
+            token = r->token;
+            lexeme = s;
+        }
+    }
+    return {token, lexeme};
+}
+
+int main()
+{
+}
