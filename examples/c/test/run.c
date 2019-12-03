4a433fee2f8588f85c7e2d55ec3a0d2b5404cb70
lggruspe <mail.levig@gmail.com>
2019-12-03 16:49:16 +0800

    Add longest match and tokenizer to run.c

--- examples/c/test/run.c
+++ examples/c/test/run.c
@@ -1,5 +1,30 @@
 #include "../output/scanners.h"
+#include <stdio.h>
 
-int main()
+void run_longest_match()
+{
+    struct scan_output s = match_longest(stdin, ALL_RECOGNIZERS);
+    print_token(stdout, s.token);
+    printf(" \n%s\n", s.lexeme);
+    free(s.lexeme);
+}
+
+// TODO run_single_match
+
+void run_tokenizer()
+{
+    for (;;) {
+        struct scan_output s = match_longest(stdin, ALL_RECOGNIZERS);
+        if (s.token == TOKEN_EMPTY) {
+            free(s.lexeme);
+            break;
+        }
+        print_token(stdout, s.token);
+        printf(" %s\n", s.lexeme);
+        free(s.lexeme);
+    }
+}
+
+int main(int argc, char **argv)
 {
 }
