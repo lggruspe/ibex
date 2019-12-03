715355afad6809a4ca05c6a0e51164ce082f056b
lggruspe <mail.levig@gmail.com>
2019-12-03 15:53:14 +0800

    Add recognizer matcher

--- examples/c/templates/template.c
+++ examples/c/templates/template.c
@@ -1,7 +1,9 @@
 #pragma once
+#include <lggds/vector.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define create_recognizer(...) (struct recognizer) { \
     .token = TOKEN_EMPTY, \
@@ -48,3 +50,45 @@ struct recognizer {
 
 {% include "scanner.c" %}
 {%- endfor %}
+
+struct match_output {
+    bool ok;
+    char *lexeme;   // must be freed by match caller
+};
+
+vector_register(vint, int)
+vector_register(vuint32_t, uint32_t)
+
+struct match_output match(FILE *fp, struct recognizer *rec)
+{
+    vector(vint) checkpoint;
+    vector(vuint32_t) lexeme;
+    checkpoint = vector_push(vint, checkpoint, 0);
+    uint32_t a = -1;
+    uint32_t eof = EOF;
+    while (vector_peek(vint, checkpoint) != rec->error && (a = fgetc(fp)) != eof) {
+        struct transition_output t = rec->transition(vector_peek(vint, checkpoint), a);
+        if (t.status == 1) {
+            rec->accept = true;
+            checkpoint = vector_destroy(vint, checkpoint);
+        }
+        checkpoint = vector_push(vint, checkpoint, t.next_state);
+        lexeme = vector_push(vuint32_t, lexeme, a);
+    }
+    for (int i = checkpoint.size; i > 1; --i) {
+        uint32_t a = vector_peek(vuint32_t, lexeme);
+        lexeme = vector_pop(vuint32_t, lexeme);
+        fputc(a, fp);
+    }
+    char *s = malloc(sizeof(char) * (lexeme.size+1));
+    s[lexeme.size] = '\0';
+    for (int i = 0; i < lexeme.size; ++i) {
+        s[i] = (char)(vector_get(vuint32_t, lexeme, i));
+    }
+    vector_destroy(vint, checkpoint);
+    vector_destroy(vuint32_t, lexeme);
+    return (struct match_output){
+        .ok = rec->accept,
+        .lexeme = s
+    };
+}
