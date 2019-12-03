dc858623faea865800788f0cd0dde7b691891a35
lggruspe <mail.levig@gmail.com>
2019-12-03 16:58:31 +0800

    Add single match runner to run.c

--- examples/c/test/run.c
+++ examples/c/test/run.c
@@ -1,5 +1,8 @@
 #include "../output/scanners.h"
+#include "../output/scanner_utils.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void run_longest_match()
 {
@@ -9,7 +12,10 @@ void run_longest_match()
     free(s.lexeme);
 }
 
-// TODO run_single_match
+void run_single_match(char *name)
+{
+    pardir_output_scanner_utils_h();
+}
 
 void run_tokenizer()
 {
@@ -27,4 +33,19 @@ void run_tokenizer()
 
 int main(int argc, char **argv)
 {
+    if (argc < 2) {
+        fprintf(stderr, "usage: run {single,longest,tokenizer} ...\n");
+        return EXIT_FAILURE;
+    }
+    if (strcmp(argv[1], "single") == 0) {
+        if (argc < 4) {
+            fprintf(stderr, "usage: run single -s SCANNER\n");
+            return EXIT_FAILURE;
+        }
+        run_single_match(argv[3]);
+    } else if (strcmp(argv[1], "longest") == 0) {
+        run_longest_match();
+    } else if (strcmp(argv[1], "tokenizer") == 0) {
+        run_tokenizer();
+    }
 }
