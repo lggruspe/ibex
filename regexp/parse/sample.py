6c57f913b7fa77c1a910216cceb3749928dc3a83
lggruspe <mail.levig@gmail.com>
2019-12-24 14:37:28 +0800

    Add sample grammar definition

---
+++ regexp/parse/sample.py
@@ -0,0 +1,14 @@
+grammar = """
+expr -> expr pipe term
+expr -> term
+term -> term factor
+term -> factor
+factor -> value star
+factor -> value plus
+factor -> value question
+factor -> value
+value -> lparen expr rparen
+value -> dot
+value -> symbol
+value -> interval
+"""
