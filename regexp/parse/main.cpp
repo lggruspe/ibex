809ede8970bc657c01d022dadbfc9d54d278d941
lggruspe <mail.levig@gmail.com>
2019-12-31 14:02:53 +0800

    Move regexp bnf to main.cpp

--- regexp/parse/main.cpp
+++ regexp/parse/main.cpp
@@ -79,6 +79,21 @@ using namespace scanner;
 
 int main()
 {
+    /*
+    start -> expr
+    expr -> expr pipe term
+    expr -> term
+    term -> term factor
+    term -> factor
+    factor -> value star
+    factor -> value plus
+    factor -> value question
+    factor -> value
+    value -> lparen expr rparen
+    value -> dot
+    value -> symbol
+    value -> interval
+    */
     Grammar<Variable, Token> g({
         {Variable::START,   {Variable::EXPR}},
         {Variable::EXPR,    {Variable::EXPR, Token::PIPE, Variable::TERM}},
