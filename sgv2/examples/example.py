36fba54d86f2108c635a722e048dcf873934149c
lggruspe <mail.levig@gmail.com>
2020-01-05 22:45:16 +0800

    Fail number and character recognizers (sgv2)

--- sgv2/examples/example.py
+++ sgv2/examples/example.py
@@ -10,10 +10,10 @@ class Scanner:
     identifier = "[_a-zA-Z][0-9a-zA-Z_]*"
     whitespace = " |\t|\n"
     __integer = "0|[1-9][0-9]*"
-    number = rf"({__integer})(\.[0-9]+)?([eE][\-\+]?({__integer}))?"
+    number = rf"({__integer})(\.[0-9]+)?([eE][\-\+]?({__integer}))?"    # TODO fix
     __cescaped = r"\\('|\\|t|n)"
     __character = rf"[\x20-\x26]|[\x28-\x5b]|[\x5d-\x7e]|({__cescaped})"
-    character = "'({__character})'"
+    character = "'({__character})'" # TODO fix
     __sescaped = r"\\[\x20-\x7e]"
     __string = rf"[\x20-\x21]|[\x23-\x5b]|[\x5d-\x7e]|({__sescaped})"
     string = f'"({__string})*"'
