b1665c4435642dac59426fbd9d6129c82d087326
lggruspe <mail.levig@gmail.com>
2020-01-25 15:39:52 +0800

    Use local scangen in sagl

--- sagl/tools/generate_scanner.py
+++ sagl/tools/generate_scanner.py
@@ -1,3 +1,9 @@
+import os
+import sys
+
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../scangen"))
+sys.path.append(path)
+
 from scangen import generate, from_class
 
 class Scanner:
