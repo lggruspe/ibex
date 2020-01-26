9b2e89babdafe54e0f4466125491ac9cde8b22b9
lggruspe <mail.levig@gmail.com>
2020-01-26 22:04:55 +0800

    Update sagl scanner generator

--- sagl/tools/generate_scanner.py
+++ sagl/tools/generate_scanner.py
@@ -1,15 +1,23 @@
-import os
+import os.path as op
 import sys
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../scangen"))
-sys.path.append(path)
+sys.path.append(op.abspath(op.join(op.dirname(__file__), "../../scangen")))
 
-from scangen import generate, from_class
+from scangen import render, from_class
 
 class Scanner:
     a = 'a'
     b = 'b'
     whitespace = "[ \t\n]"
 
+config = None
+
+def main():
+    code = render(from_class(Scanner), "template.cpp", config=config)
+    relpath = "../src/scanner.hpp"
+    abspath = op.abspath(op.join(op.dirname(__file__), relpath))
+    with open(abspath, "w") as f:
+        print(code, file=f)
+
 if __name__ == "__main__":
-    generate(from_class(Scanner))
+    main()
