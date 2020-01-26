3451ceb7a4a3dda4b86e4ace93430e02a9192750
lggruspe <mail.levig@gmail.com>
2020-01-26 21:11:19 +0800

    Update scangen test script to generate and cleanup template output

--- scangen/test/test.sh
+++ scangen/test/test.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+source ../env/bin/activate
+export LD_LIBRARY_PATH=/usr/local/lib
+python example.py
+make -B -C c
+make -B -C cpp
+make -B -C dot
+make -B -C python
 export TEST_DIR=c
 python test.py
 export TEST_DIR=cpp
 python test.py
 export TEST_DIR=python
 python test.py
+make clean -C c
+make clean -C cpp
+make clean -C dot
+make clean -C python
