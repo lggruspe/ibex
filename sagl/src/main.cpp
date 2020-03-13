593b319ee16bfdef389ecab0a664bf2aba941143
lggruspe <mail.levig@gmail.com>
2020-03-14 00:48:17 +0800

    Delete unused code in sagl example

--- sagl/src/main.cpp
+++ sagl/src/main.cpp
@@ -1,48 +1,8 @@
 #include "sagl.hpp"
-#include "scanner.hpp"
-#include <iostream>
-
-std::ostream& operator<<(std::ostream& os, Item item)
-{
-    os << "{" << item.rule.first << " ->";
-    for (int i = 0; i < (int)(item.rule.second.size()); ++i) {
-        if (i == item.position) {
-            os << " .";
-        }
-        os << " " << item.rule.second[i];
-    }
-    if (item.is_complete()) {
-        os << " .";
-    }
-    return os << ", " << item.lookahead << "}";
-}
-
-std::ostream& operator<<(
-    std::ostream& os,
-    const Automaton<Grammar>::State& state)
-{
-    for (const auto& item: state) {
-        os << item << std::endl;
-    }
-    return os;
-}
-
-std::ostream& operator<<(
-    std::ostream& os,
-    const Automaton<Grammar>& m)
-{
-    for (const auto& [state, id]: m) {
-        os << "CC" << id << ":" << std::endl;
-        for (const auto& item: state) {
-            os << item << std::endl;
-        }
-        os << std::endl;
-    }
-    return os;
-}
-
 #include "parser.hpp"
 #include "parsetree.hpp"
+#include "scanner.hpp"
+#include <iostream>
 
 int main()
 {
