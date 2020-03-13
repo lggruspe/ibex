66dd2466b6f47c87ec7a37de89a831b55203aa17
lggruspe <mail.levig@gmail.com>
2020-03-13 18:46:12 +0800

    Refactor test script

--- scangen/test/test.sh
+++ scangen/test/test.sh
@@ -1,18 +1,14 @@
 #!/usr/bin/env bash
 
 source ../env/bin/activate
-export LD_LIBRARY_PATH=/usr/local/lib
-python generate_code.py
+LD_LIBRARY_PATH=/usr/local/lib python generate_code.py
 make -B -C c
 make -B -C cpp
 make -B -C dot
 make -B -C python
-export TEST_DIR=c
-python test.py
-export TEST_DIR=cpp
-python test.py
-export TEST_DIR=python
-python test.py
+TEST_DIR=c python test.py
+TEST_DIR=cpp python test.py
+TEST_DIR=python python test.py
 make clean -C c
 make clean -C cpp
 make clean -C dot
