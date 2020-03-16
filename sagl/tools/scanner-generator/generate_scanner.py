0ecf94b4ae4afe8af5c7f35b9a40152390caae2f
lggruspe <mail.levig@gmail.com>
2020-03-17 00:24:05 +0800

    Update scanner generator for sagl example

--- sagl/tools/scanner-generator/generate_scanner.py
+++ sagl/tools/scanner-generator/generate_scanner.py
@@ -1,22 +1,25 @@
-import os.path as op
+from os.path import abspath, dirname, join
 import sys
 
-sys.path.append(op.abspath(op.join(op.dirname(__file__), "../../../scangen")))
+BASEDIR = abspath(dirname(__file__))
+sys.path.append(abspath(join(dirname(__file__), "../../../codegen")))
+sys.path.append(abspath(join(dirname(__file__), "../../../rnd/python")))
 
-from scangen import render, from_class
+from codegen import render
+from rnd import convert, from_class
 
 class Scanner:
     a = 'a'
     b = 'b'
     whitespace = "[ \t\n]"
 
-config = None
-
 def main():
-    code = render(from_class(Scanner), "template.cpp", config=config)
+    code = render("template.cpp", context={
+        "scanner": convert(from_class(Scanner)),
+        "config": None,
+    })
     relpath = "../../src/scanner.hpp"
-    abspath = op.abspath(op.join(op.dirname(__file__), relpath))
-    with open(abspath, "w") as f:
+    with open(abspath(join(dirname(__file__), relpath)), "w") as f:
         print(code, file=f)
 
 if __name__ == "__main__":
