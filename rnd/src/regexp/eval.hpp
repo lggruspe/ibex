673d2f75ecd70fee51c4ed94407e4207f7fa466f
lggruspe <mail.levig@gmail.com>
2020-03-19 00:13:36 +0800

    Replace regexp evaluation engine

--- rnd/src/regexp/eval.hpp
+++ rnd/src/regexp/eval.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "regexp/parser/scanner.hpp"
+#include "regexp/parser/parser.hpp"
 #include "rnd/automaton.hpp"
 #include "rnd/nexpr.hpp"
 #include "rnd/zsymbols.hpp"
@@ -42,172 +43,203 @@ uint32_t to_symbol(const std::string& lexeme)
     }
 }
 
-template <class Symbol>
-struct Callback {
-    std::vector<rnd::NExpr> state;
+struct Context {
+    using Symbol = std::string;
+    using Rule = std::pair<Symbol, std::vector<Symbol>>;
+
+    std::vector<rnd::NExpr> stack;
+   
+    std::vector<rnd::NExpr> get_rhs(const Rule& rule)
+    {
+        if (rule.second.size() > stack.size()) {
+            throw std::logic_error("invalid reduce");
+        }
+        std::vector<rnd::NExpr> rhs(
+            stack.begin() + (stack.size() - rule.second.size()),
+            stack.end());
+        for (int i = 0; i < (int)(rule.second.size()); ++i) {
+            stack.pop_back();
+        }
+        return rhs;
+    }
+};
+
+bool shift_all(Context& self, const std::pair<std::string, std::string>& lookahead)
+{
+    auto [token, lexeme] = lookahead;
+    const static std::set<std::string> nulls{
+        "pipe", "star", "lparen", "rparen", "question", "plus", "lbracket",
+        "rbracket", "dash",
+    };
+    if (nulls.count(std::string(token))) {
+        self.stack.push_back(rnd::NExpr(rnd::ZRange(lexeme[0], lexeme[0]+1)));
+    } else if (std::string(token) == std::string("dot")) {
+        auto limit = std::numeric_limits<uint32_t>::max();
+        self.stack.push_back(rnd::NExpr(rnd::ZRange(0, limit)));
+    } else if (std::string(token) == std::string("symbol")) {
+        auto a = to_symbol(lexeme);
+        self.stack.push_back(rnd::NExpr(rnd::ZRange(a, a+1)));
+    } else {
+        return false;
+    }
+    return true;
+}
+
+using Rule = std::pair<std::string, std::vector<std::string>>;
+
+bool reduce_expr(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (auto size = rhs.size(); size == 1) {
+        self.stack.push_back(rhs[0]);
+    } else if (size == 3) {
+        self.stack.push_back(rnd::alternate(rhs[0], rhs[2]));
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+bool reduce_term(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (auto size = rhs.size(); size == 1) {
+        self.stack.push_back(rhs[0]);
+    } else if (size == 2) {
+        self.stack.push_back(rnd::concatenate(rhs[0], rhs[1]));
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+bool reduce_factor(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (auto size = rhs.size(); size == 1) {
+        self.stack.push_back(rhs[0]);
+    } else if (size == 2) {
+        auto a = rhs[1].states.at(0).begin()->first;
+        switch (a.start) {
+        case '*':
+            self.stack.push_back(rnd::closure(rhs[0]));
+            break;
+        case '?': {
+            self.stack.push_back(rnd::alternate(rhs[0], rnd::epsilon()));
+            break;
+        }
+        case '+':
+            self.stack.push_back(rnd::concatenate(rhs[0], rnd::closure(rhs[0])));
+            break;
+        default:
+            throw std::logic_error("invalid reduce");
+        }
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+bool reduce_value(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (auto size = rhs.size(); size == 1) {
+        self.stack.push_back(rhs[0]);
+    } else if (size == 3) {
+        self.stack.push_back(rhs[1]);
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+bool reduce_simple(Context& self, const Rule& rule)
+{
+    return true;
+}
+
+bool reduce_compound(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (rhs.size() == 3) {
+        auto a = rhs[0].states.at(0).begin()->first.start;
+        auto b = rhs[2].states.at(0).begin()->first.start;
+        if ((a == '(' && b == ')') || (a == '[' && b == ']')) {
+            self.stack.push_back(rhs[1]);
+        } else {
+            throw std::logic_error("invalid reduce");
+        }
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+bool reduce_list(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (auto size = rhs.size(); size == 1) {
+        self.stack.push_back(rhs[0]);
+    } else if (size == 2) {
+        self.stack.push_back(rnd::alternate(rhs[0], rhs[1]));
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+bool reduce_element(Context& self, const Rule& rule)
+{
+    auto rhs = self.get_rhs(rule);
+    if (auto size = rhs.size(); size == 1) {
+        self.stack.push_back(rhs[0]);
+    } else if (size == 3) {
+        auto a = rhs[0].states.at(0).begin()->first.start;
+        auto b = rhs[2].states.at(0).begin()->first.end;
+        self.stack.push_back(rnd::ZRange(a, b));
+    } else {
+        throw std::logic_error("invalid reduce");
+    }
+    return true;
+}
+
+struct Callback: public parser::BaseCallback<Context> {
+    Callback()
+    {
+        reduce("expr -> expr pipe term", reduce_expr);
+        reduce("expr -> term", reduce_expr);
+        reduce("term -> term factor", reduce_term);
+        reduce("term -> factor", reduce_term);
+        reduce("factor -> value star", reduce_factor);
+        reduce("factor -> value plus", reduce_factor);
+        reduce("factor -> value question", reduce_factor);
+        reduce("factor -> value", reduce_factor);
+        reduce("value -> simple", reduce_value);
+        reduce("value -> compound", reduce_value);
+        reduce("simple -> dot", reduce_simple);
+        reduce("simple -> symbol", reduce_simple);
+        reduce("compound -> lparen expr rparen", reduce_compound);
+        reduce("compound -> lbracket list rbracket", reduce_compound);
+        reduce("list -> list element", reduce_list);
+        reduce("list -> element", reduce_list);
+        reduce("element -> symbol", reduce_element);
+        reduce("element -> symbol dash symbol", reduce_element);
+    }
 
     auto accept(bool acc)
     {
         if (!acc) {
             throw std::logic_error("syntax error");
         }
-        return rnd::Automaton(state.back());
+        return rnd::Automaton(state.stack.back());
     }
 
-    void shift(const std::pair<char const*, std::string>& lookahead)
+    bool handle_shift(const std::pair<std::string, std::string>& lookahead)
     {
-        auto [token, lexeme] = lookahead;
-        std::set<std::string> nulls{
-            "pipe", "star", "lparen", "rparen", "question", "plus", "lbracket",
-            "rbracket", "dash",
-        };
-        if (nulls.count(std::string(token))) {
-            state.push_back(rnd::NExpr(rnd::ZRange(lexeme[0], lexeme[0]+1)));
-        } else if (std::string(token) == std::string("dot")) {
-            auto limit = std::numeric_limits<uint32_t>::max();
-            state.push_back(rnd::NExpr(rnd::ZRange(0, limit)));
-        } else if (std::string(token) == std::string("symbol")) {
-            auto a = to_symbol(lexeme);
-            state.push_back(rnd::NExpr(rnd::ZRange(a, a+1)));
-        } else {
+        if (bool ok = shift_all(state, lookahead); !ok) {
             throw std::logic_error("invalid shift");
         }
-    }
-
-    void reduce_expr(std::vector<rnd::NExpr>& rhs)
-    {
-        if (auto size = rhs.size(); size == 1) {
-            state.push_back(rhs[0]);
-        } else if (size == 3) {
-            state.push_back(rnd::alternate(rhs[0], rhs[2]));
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    void reduce_term(std::vector<rnd::NExpr>& rhs)
-    {
-        if (auto size = rhs.size(); size == 1) {
-            state.push_back(rhs[0]);
-        } else if (size == 2) {
-            state.push_back(rnd::concatenate(rhs[0], rhs[1]));
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    void reduce_factor(std::vector<rnd::NExpr>& rhs)
-    {
-        if (auto size = rhs.size(); size == 1) {
-            state.push_back(rhs[0]);
-        } else if (size == 2) {
-            auto a = rhs[1].states.at(0).begin()->first;
-            switch (a.start) {
-            case '*':
-                state.push_back(rnd::closure(rhs[0]));
-                break;
-            case '?': {
-                state.push_back(rnd::alternate(rhs[0], rnd::epsilon()));
-                break;
-            }
-            case '+':
-                state.push_back(rnd::concatenate(rhs[0], rnd::closure(rhs[0])));
-                break;
-            default:
-                throw std::logic_error("invalid reduce");
-            }
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    void reduce_value(std::vector<rnd::NExpr>& rhs)
-    {
-        if (auto size = rhs.size(); size == 1) {
-            state.push_back(rhs[0]);
-        } else if (size == 3) {
-            state.push_back(rhs[1]);
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    void reduce_simple(std::vector<rnd::NExpr>& rhs)
-    {
-        state.push_back(rhs[0]);
-    }
-
-    void reduce_compound(std::vector<rnd::NExpr>& rhs)
-    {
-        if (rhs.size() == 3) {
-            auto a = rhs[0].states.at(0).begin()->first.start;
-            auto b = rhs[2].states.at(0).begin()->first.start;
-            if ((a == '(' && b == ')') || (a == '[' && b == ']')) {
-                state.push_back(rhs[1]);
-            } else {
-                throw std::logic_error("invalid reduce");
-            }
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    void reduce_list(std::vector<rnd::NExpr>& rhs)
-    {
-        if (auto size = rhs.size(); size == 1) {
-            state.push_back(rhs[0]);
-        } else if (size == 2) {
-            state.push_back(rnd::alternate(rhs[0], rhs[1]));
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    void reduce_element(std::vector<rnd::NExpr>& rhs)
-    {
-        if (auto size = rhs.size(); size == 1) {
-            state.push_back(rhs[0]);
-        } else if (size == 3) {
-            auto a = rhs[0].states.at(0).begin()->first.start;
-            auto b = rhs[2].states.at(0).begin()->first.end;
-            state.push_back(rnd::ZRange(a, b));
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
-    }
-
-    using Rule = std::pair<Symbol, std::vector<Symbol>>;
-    void reduce(const Rule& rule)
-    {
-        if (rule.second.size() > state.size()) {
-            throw std::logic_error("invalid reduce");
-        }
-        std::vector<rnd::NExpr> rhs(
-            state.begin() + (state.size() - rule.second.size()),
-            state.end());
-        for (int i = 0; i < (int)(rule.second.size()); ++i) {
-            state.pop_back();
-        }
-        auto lhs = rule.first;
-        if (std::string(lhs) == std::string("expr")) {
-            reduce_expr(rhs);
-        } else if (std::string(lhs) == std::string("term")) {
-            reduce_term(rhs);
-        } else if (std::string(lhs) == std::string("factor")) {
-            reduce_factor(rhs);
-        } else if (std::string(lhs) == std::string("value")) {
-            reduce_value(rhs);
-        } else if (std::string(lhs) == std::string("simple")) {
-            reduce_simple(rhs);
-        } else if (std::string(lhs) == std::string("compound")) {
-            reduce_compound(rhs);
-        } else if (std::string(lhs) == std::string("list")) {
-            reduce_list(rhs);
-        } else if (std::string(lhs) == std::string("element")) {
-            reduce_element(rhs);
-        } else {
-            throw std::logic_error("invalid reduce");
-        }
+        return true;
     }
 };
 
