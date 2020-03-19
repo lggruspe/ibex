7ea5c803151ba42fbac071e26a43f4c33b4c205c
lggruspe <mail.levig@gmail.com>
2020-03-19 14:46:55 +0800

    Merge grammar_parser into sagl target

--- sagl/src/sagl/grammar-parser/grammar_parser.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <string>
-
-std::string parse_grammar(const std::string& grammar);
--- sagl/src/sagl/sagl.cxx
+++ sagl/src/sagl/sagl.cxx
@@ -1,7 +1,21 @@
 #include "sagl.h"
-#include "grammar-parser/grammar_parser.h"
+#include "sagl.hpp"
+#include "parser/parser.hpp"
+#include "parser/eval.hpp"
 #include <cstdlib>
 #include <cstring>
+#include <string>
+
+std::string parse_grammar(const std::string& s)
+{
+    // can throw SyntaxError
+    ParseTreeCallback cb;
+    bool ok = parser::parse(s, &cb);
+    auto [start, rules] = cb.accept(ok);
+    Grammar g(start, rules);
+    Table t(g);
+    return jsonify(t);
+}
 
 char const *sagl_open(char const* bnf)
 {
