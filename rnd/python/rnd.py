3d0f7b1aa9d7e7d5c023ec19b0bf87dde18f922d
lggruspe <mail.levig@gmail.com>
2020-03-19 11:39:54 +0800

    Rename regexp to rnd

--- rnd/python/rnd.py
+++ rnd/python/rnd.py
@@ -1,18 +1,18 @@
 import ctypes
 import json
 
-libregexp = ctypes.CDLL("libregexp.so")
-libregexp.regexp_open.argtypes = [ctypes.c_char_p]
-libregexp.regexp_open.restype = ctypes.POINTER(ctypes.c_char)
-libregexp.regexp_close.argtypes = [ctypes.c_void_p]
-libregexp.regexp_close.restype = None
+librnd = ctypes.CDLL("librnd.so")
+librnd.rnd_open.argtypes = [ctypes.c_char_p]
+librnd.rnd_open.restype = ctypes.POINTER(ctypes.c_char)
+librnd.rnd_close.argtypes = [ctypes.c_void_p]
+librnd.rnd_close.restype = None
 
 class DFA:
     def __init__(self, s):
         self.regex = s
-        re = libregexp.regexp_open(s.encode("utf-8"))
+        re = librnd.rnd_open(s.encode("utf-8"))
         js = json.loads(ctypes.cast(re, ctypes.c_char_p).value)
-        libregexp.regexp_close(re)
+        librnd.rnd_close(re)
         self.start = js["start"]
         self.accepts = set(js["accepts"])
         self.transitions = {}
--- rnd/tools/generate.sh
+++ rnd/tools/generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source env/bin/activate
-LD_LIBRARY_PATH=../../sagl/build python generate_scanner.py
+LD_LIBRARY_PATH="../build:../../sagl/build" python generate_scanner.py
