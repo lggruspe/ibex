8870ad2a69efb3bbf31d14141a791a8872266133
Levi Gruspe <mail.levig@gmail.com>
2020-04-02 22:03:00 +0800

    Add parser for new scanner

---
+++ python/codegen/templates/cparser/parser.cpp
@@ -0,0 +1,90 @@
+#include "scanner.h"
+#include "parser.h"
+#include <map>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+
+const std::set<std::string> ignores = {
+    "whitespace",
+};
+
+enum class Action { ERROR = 0, GOTO, SHIFT, REDUCE, ACCEPT };
+
+std::map<int, std::map<std::string, std::pair<Action, int>>> parse_table {
+    {%- for action in table %}
+    {
+        {{ loop.index0 }}, {
+            {%- for entry in table[loop.index0] %}
+            {"{{ entry.symbol }}", {Action::{{ entry.type|upper }}, {{ entry.value }}}},
+            {%- endfor %}
+        }
+    },
+    {%- endfor %}
+};
+
+// NULL terminated
+std::vector<std::pair<char const*, std::vector<char const*>>> rules {
+    {%- for rule in grammar %}
+    {"{{ rule.lhs }}", { {% for word in rule.rhs %}"{{ word }}", {% endfor %}nullptr }},
+    {%- endfor %}
+};
+
+std::pair<std::string, std::string> scan(const char*& text)
+{
+    int n;
+    std::string tok = match_longest(text, &n);
+    while (ignores.count(tok)) {
+        text += n;
+        tok = match_longest(text, &n);
+    }
+    if (tok.empty()) {
+        tok = *text ? "error" : "empty";
+    }
+    std::string lex(text, n);
+    text += n;
+    return {tok, lex};
+}
+
+bool parse(
+    char const *text,
+    bool (*shift)(void*, char const*, char const*),
+    bool (*reduce)(void*, char const*, char const* const*),
+    void *arg)
+{
+    std::vector<int> states {0};
+    auto lookahead = scan(text);
+    for (;;) {
+        auto tok = lookahead.first;
+        auto lex = lookahead.second;
+        auto action = parse_table[states.back()][tok];
+        switch (action.first) {
+        case Action::ACCEPT:
+            return true;
+        case Action::SHIFT:
+            if (shift && !shift(arg, tok.c_str(), lex.c_str())) {
+                return false;
+            }
+            states.push_back(action.second);
+            lookahead = scan(text);
+            break;
+        case Action::REDUCE: {
+            const auto& rule = rules[action.second];
+            if (reduce && !reduce(arg, rule.first, &rule.second[0])) {
+                return false;
+            }
+            for (const auto& s: rule.second) {
+                if (!s) {
+                    break;
+                }
+                states.pop_back();
+            }
+            states.push_back(parse_table[states.back()][rule.first].second);
+            break;
+        }
+        default:
+            return false;
+        }
+    }
+}
---
+++ tools/cparser/generate.py
@@ -0,0 +1,46 @@
+from os.path import abspath, dirname, join, pardir
+import sys
+
+BASEDIR = abspath(join(dirname(__file__), pardir, pardir))
+sys.path.append(join(BASEDIR, "python"))
+sys.path.append(join(BASEDIR, "python/codegen"))
+
+import codegen
+import rnd
+import sagl
+
+class Scanner:
+    a = "a"
+    b = "b"
+    whitespace = "[ \t\n]"
+
+grammar = """
+S -> A.
+A -> a A b.
+A -> .
+"""
+
+scanner_h = codegen.render("cscanner/scanner.h", context={})
+
+scanner_c = codegen.render("cscanner/scanner.c", context={
+    "scanner": rnd.convert(rnd.from_class(Scanner)),
+    "config": None,
+})
+
+parser_h = codegen.render("cparser/parser.h", context={})
+
+parser_cpp = codegen.render("cparser/parser.cpp", context={
+    **sagl.parse_grammar(grammar),
+})
+
+with open("scanner.h", "w") as f:
+    print(scanner_h, file=f)
+
+with open("scanner.c", "w") as f:
+    print(scanner_c, file=f)
+
+with open("parser.h", "w") as f:
+    print(parser_h, file=f)
+
+with open("parser.cpp", "w") as f:
+    print(parser_cpp, file=f)
